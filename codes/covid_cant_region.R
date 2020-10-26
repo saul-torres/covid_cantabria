77d3750eede886a3ba113c4cd70a4f92173ea46f
saul-torres <12624342+saul-torres@users.noreply.github.com>
2020-10-26 22:25:13 +0100

    25-oct data

--- codes/covid_cant_region.R
+++ codes/covid_cant_region.R
@@ -4,7 +4,8 @@ library(jsonlite)
 
 # Regional population
 cant_popul <- 581078
-
+hosp_beds <- 1551
+ucis_beds <- 115
 
 
 # DATA SCRAPPING
@@ -42,16 +43,38 @@ region_test_values <- as.data.frame(t(region_test_values))
 region_test_df <- as.data.frame(cbind(as.character(region_test_json$dimension$Fecha$category$label),
                                       region_test_values))
 
+region_hosp_url <- "https://covid19can-data.firebaseio.com/saludcantabria/hospitalizations.json"
+region_hosp_url <- URLencode(region_hosp_url)
+region_hosp_json <- fromJSON(region_hosp_url)
+region_hosp_values <- as.data.frame(split(region_hosp_json$value, ceiling(seq_along(region_hosp_json$value)/5)))
+region_hosp_values <- as.data.frame(t(region_hosp_values))
+region_hosp_df <- as.data.frame(cbind(as.character(region_hosp_json$dimension$Fecha$category$label),
+                                      region_hosp_values))
+region_hosp_df$total <- region_hosp_df$V1+region_hosp_df$V2+region_hosp_df$V3+region_hosp_df$V4+region_hosp_df$V5
+
+region_ucis_url <- "https://covid19can-data.firebaseio.com/saludcantabria/ucis.json"
+region_ucis_url <- URLencode(region_ucis_url)
+region_ucis_json <- fromJSON(region_ucis_url)
+region_ucis_values <- as.data.frame(split(region_ucis_json$value, ceiling(seq_along(region_ucis_json$value)/2)))
+region_ucis_values <- as.data.frame(t(region_ucis_values))
+region_ucis_df <- as.data.frame(cbind(as.character(region_ucis_json$dimension$Fecha$category$label),
+                                      region_ucis_values))
+region_ucis_df$total <- region_ucis_df$V1+region_ucis_df$V2
+
+
 region_datos_df <- cbind(region_accumulat_df,
                          region_daily_df[2],
                          region_deceased_df[2],
                          region_recovered_df[2],
-                         region_test_df[c(2,3)])
+                         region_test_df[c(2,3)],
+                         region_hosp_df$total,
+                         region_ucis_df$total)
 
 colnames(region_datos_df) <- c("date", "total_cases", "total_deceased", "total_recovered",
                                "uci_cases",
                                "new_cases", "new_deceased", "new_recovered",
-                               "total_antic", "total_pcr")
+                               "total_antic", "total_pcr",
+                               "daily_hosp", "daily_uci")
 region_datos_df$date <- as.Date(region_datos_df$date, format = "%Y-%m-%d")
 region_datos_df$uci_cases <- as.numeric(region_datos_df$uci_cases)
 region_datos_df$new_cases <- as.numeric(region_datos_df$new_cases)
@@ -82,6 +105,9 @@ for (fecha_index in c(8:dim(region_datos_df)[1])){
 }
 region_datos_df$tasa_pcr_7d <- serie_tasa_pcr_7d*100
 
+region_datos_df$tasa_hosp_ocup <- 100*region_datos_df$daily_hosp/hosp_beds
+region_datos_df$tasa_ucis_ocup <- 100*region_datos_df$daily_uci/ucis_beds
+
 
 write.csv2(region_datos_df, "data/regional_data.csv")
 
@@ -106,11 +132,6 @@ p_incid <- ggplot(region_datos_df, aes(date, incid_14d)) +
   geom_hline(yintercept=250, alpha=.75, size=0.25, colour="#feb24c") +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,600)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(title = paste("EVOLUCIÓN COVID19 EN CANTABRIA (", fecha_dia_plot, ")", sep=""),
        subtitle = "INCIDENCIA (acumulado 14 días)",
        x = "",
@@ -126,11 +147,6 @@ p_active <- ggplot(region_datos_df, aes(date, active)) +
                          colours = c("#bdbdbd", "#feb24c", "#de2d26", "#a50f15")) +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,3000)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(subtitle = "CASOS ACTIVOS",
        x = "",
        y = "Casos activos",
@@ -144,26 +160,6 @@ p_dailyd <- ggplot(region_datos_df, aes(date, new_deceased)) +
                          colours=c("#636363", "#bdbdbd", "#de2d26")) +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,20)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
-  geom_text(x=as.Date("04/05/2020", format = "%d/%m/%Y"), y=17.5,
-            label="Inicio Fase 0", colour="#a50f15",
-            angle = 90, vjust = "inward", hjust = "inward") + 
-  geom_text(x=as.Date("11/05/2020", format = "%d/%m/%Y"), y=17.5,
-            label="Inicio Fase 1", colour="#de2d26",
-            angle = 90, vjust = "inward", hjust = "inward") + 
-  geom_text(x=as.Date("25/05/2020", format = "%d/%m/%Y"), y=17.5,
-            label="Inicio Fase 2", colour="#fb6a4a",
-            angle = 90, vjust = "inward", hjust = "inward") + 
-  geom_text(x=as.Date("08/06/2020", format = "%d/%m/%Y"), y=17.5,
-            label="Inicio Fase 3", colour="#fc9272",
-            angle = 90, vjust = "inward", hjust = "inward") + 
-  geom_text(x=as.Date("19/06/2020", format = "%d/%m/%Y"), y=17.5,
-            label="Nueva normalidad", colour="#addd8e",
-            angle = 90, vjust = "inward", hjust = "inward") + 
   labs(subtitle = "FALLECIMIENTOS DIARIOS",
        #caption = "Datos: ICANE",
        x = "",
@@ -179,11 +175,6 @@ p_pcr <- ggplot(region_datos_df, aes(date, tasa_pcr_7d)) +
                          colours = c("#bdbdbd", "#feb24c", "#de2d26", "#a50f15")) +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,20)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(subtitle = "% de test PCR positivos (promedio 7 días)",
        x = "",
        y = "Porc.",
@@ -200,17 +191,24 @@ p_test <- ggplot(test_data, aes(x=date, y=value, fill=variable)) +
   geom_bar(position="stack", stat="identity") + 
   scale_fill_manual(labels = c("PCR", "Otros"), values=c("#de2d26", "#bdbdbd")) +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(subtitle = "Número de pruebas realizadas",
        x = "Fecha",
        y = "Número de pruebas",
        fill = "Tipo de prueba") +  
   theme(legend.position = "bottom")
 
+# ocupación de camas
+beds_data <- melt(region_datos_df[,c(1,19,20)], id="date")
+
+p_beds <- ggplot(beds_data, aes(x=date, y=value, colour=variable)) + 
+  geom_point() + 
+  scale_colour_manual(labels = c("Hospitalizados", "UCIS"), values=c("#bdbdbd", "#de2d26")) +
+  coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1)) +
+  labs(subtitle = "Porcentaje de camas ocupadas",
+       x = "Fecha",
+       y = "Porcentaje de ocupación",
+       fill = "Tipo de cama") +  
+  theme(legend.position = "bottom")
 
 p_combine <- grid.arrange(p_incid, p_active, p_dailyd, p_test, p_pcr,  
                           ncol = 1, heights = c(1.1, 1, 1, 1.2, 1))
@@ -230,11 +228,6 @@ p_incid_indiv <- ggplot(region_datos_df, aes(date, incid_14d)) +
   geom_hline(yintercept=250, alpha=.75, size=0.25, colour="#feb24c") +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,600)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(title = paste("EVOLUCIÓN COVID19 EN CANTABRIA (", fecha_dia_plot, ")", sep=""),
        subtitle = "INCIDENCIA (acumulado 14 días)",
        x = "",
@@ -251,11 +244,6 @@ p_active_indiv <- ggplot(region_datos_df, aes(date, active)) +
                          colours = c("#bdbdbd", "#feb24c", "#de2d26", "#a50f15")) +
   coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1),
                   ylim=c(0,3000)) +
-  geom_vline(xintercept=as.Date("04/05/2020", format = "%d/%m/%Y"), colour="#a50f15") +
-  geom_vline(xintercept=as.Date("11/05/2020", format = "%d/%m/%Y"), colour="#de2d26") +
-  geom_vline(xintercept=as.Date("25/05/2020", format = "%d/%m/%Y"), colour="#fb6a4a") +
-  geom_vline(xintercept=as.Date("08/06/2020", format = "%d/%m/%Y"), colour="#fc9272") +
-  geom_vline(xintercept=as.Date("19/06/2020", format = "%d/%m/%Y"), colour="#addd8e") +
   labs(title = paste("EVOLUCIÓN COVID19 EN CANTABRIA (", fecha_dia_plot, ")", sep=""),
        subtitle = "CASOS ACTIVOS",
        x = "",
@@ -265,10 +253,24 @@ p_active_indiv <- ggplot(region_datos_df, aes(date, active)) +
   theme(legend.position = "none")
 
 
+# CAMAS OCUPADAS
+p_beds_indiv <- ggplot(beds_data, aes(x=date, y=value, colour=variable)) + 
+  geom_point() + 
+  scale_colour_manual(labels = c("Hospitalizados", "UCIS"), values=c("#bdbdbd", "#de2d26")) +
+  coord_cartesian(xlim=c(region_datos_df$date[length(region_datos_df$date)]-60,region_datos_df$date[length(region_datos_df$date)]+1)) +
+  labs(subtitle = "Porcentaje de camas ocupadas",
+       x = "Fecha",
+       y = "Porcentaje de ocupación",
+       fill = "Tipo de cama") +  
+  theme(legend.position = "bottom")
+
+
 
 ggsave("images/regional_incid.png", p_incid_indiv,
        width = 30, height = 8, units = "cm")
 ggsave("images/regional_active.png", p_active_indiv,
+       width = 30, height = 8, units = "cm")
+ggsave("images/regional_beds.png", p_beds_indiv,
        width = 30, height = 8, units = "cm")
 
 
